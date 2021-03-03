b198e19297ec51ee035fd7630b08a2c51dfebe41
Bev Antwi <akwua8@hotmail.com>
2021-03-03 17:16:20 +0000

    got the searchbar elemnt in the header working, just need to append it to card

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import './HeaderComponent.css';
-
-// TODO - make sure HeaderComponent is expecting the right props (if any)!
-export const HeaderComponent = () => {
-    return <header className={'header'}>
-        {/* TODO [STRETCH] - add in any controls that you'd like in your header
+import React, {useState} from "react";
+import "./HeaderComponent.css";
+/* TODO [STRETCH] - add in any controls that you'd like in your header
              E.g, a search bar, a toggle button for the side bar, or just a plain header!
-        */}
-        I'm the header component
+
+        */
+// TODO - make sure HeaderComponent is expecting the right props (if any)!
+const api = {
+    characters: "https://www.anapioficeandfire.com/api/characters"
+}
+export const HeaderComponent = () => {
+
+    const [query, setQuery] = useState("");
+
+    const fetchData = async(event) => {
+        if (event.key==="Enter"){
+            fetch (`${api.characters}?name=${query}`)
+            .then(resp => resp.json())
+            .catch(error => {
+                alert("try spelling the name ocrrecty? maybe?")
+            })
+            .then(data => {
+                setQuery("");
+                console.log(data)
+        })  
+    } 
+    };
+
+  return (
+    <header className={"header"}>
+      {
+      }
+      <div className="search-bar">
+        <div>
+          <input
+            type="text"
+            className="searchbar"
+            placeholder="Enter character name..."
+            autoComplete="off"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={fetchData}
+            value={query}
+          />
+        </div>
+      </div>
+      Game of Thrones React App
     </header>
+  );
 };
