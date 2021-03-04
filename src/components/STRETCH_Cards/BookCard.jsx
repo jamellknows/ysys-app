691b9a6fe25bdfad35969c6c4c31927e6563a07b
jamellknows <jamellsamuels@googlemail.com>
2021-03-04 03:34:32 +0000

    remove folder

--- src/components/STRETCH_Cards/BookCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Card.css';
-
-// TODO - create a component which displays information about Books
-
-// TODO - make sure BookCard is expecting the right props!
-export const BookCard = () => {
-    return <div />
-};
--- src/components/STRETCH_Cards/HouseCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Card.css';
-
-// TODO - create a component which displays information about Houses
-
-// TODO - make sure HouseCard is expecting the right props!
-export const HouseCard = () => {
-    return <div />
-};
